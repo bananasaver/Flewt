23f0dd560401f561b783c9fecf536f1fcf0e74e7
bananasaver <bananasaver@proton.me>
2026-09-05 19:04:06 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-function Test() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Flewt Router is working</h1>
-      <p>React and BrowserRouter have mounted successfully.</p>
-    </div>
-  );
-}
+import App from "./App";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Test />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
